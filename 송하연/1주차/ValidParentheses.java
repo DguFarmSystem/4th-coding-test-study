6aab68a6d66803e46b5100362be27f9c7cbcad26
hanni <102026726+hysong4u@users.noreply.github.com>
2025-07-21 03:31:24 +0900

    Create ValidParentheses.java
    [250721] 올바른 괄호

---
+++ 송하연/1주차/ValidParentheses.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<String> stack = new Stack<>();
+    
+        for(int i=0; i < s.length(); i++){
+            
+            char ch = s.charAt(i);
+            
+            if(ch == '(') { // char은 작은 따옴표
+                stack.push(String.valueOf(ch)); //String 형 변환  
+            }
+            else if (ch == ')'){
+                if(stack.isEmpty()) answer = false;
+                else stack.pop();
+            }
+        }
+        
+        if(!stack.isEmpty()){
+            answer = false;
+        }
+
+        return answer;
+    }
+}
