9647f1a4cef9fe0041c4eda52855597ddf7012b1
hanni <102026726+hysong4u@users.noreply.github.com>
2025-07-21 14:55:55 +0900

    [250721] 전화번호 목록

---
+++ 송하연/1주차/PhoneNumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String num : phone_book){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        
+         for(String num : phone_book){
+    
+            for(int i = 1; i <= num.toString().length(); i++ ){
+                String sb = num.substring(0,i);
+         
+                if(map.containsKey(sb) && (sb != num)) {
+                    answer = false; 
+                    return answer;
+                }
+
+            }     
+        }
+        
+        return answer;
+    }
+}
