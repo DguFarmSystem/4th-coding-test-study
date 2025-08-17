160bb490b4f94f4f6c7d4f29b73030f3054fecc1
Heejin Yang <114398091+jjin70@users.noreply.github.com>
2025-08-18 01:23:14 +0900

    [250818] 체육복

---
+++ 양희진/5주차/체육복.py
@@ -0,0 +1,17 @@
+def solution(n, lost, reserve):
+    answer = n
+    lost.sort()
+    reserve.sort()
+	
+    for i in reserve[:]: # reverse의 복사본 순회
+        if i in lost:
+            reserve.remove(i)
+            lost.remove(i)
+            
+    for i in reserve: 
+        if i-1 in lost:
+            lost.remove(i-1)
+        elif i+1 in lost:
+            lost.remove(i+1)
+    answer = n - len(lost)
+    return answer
